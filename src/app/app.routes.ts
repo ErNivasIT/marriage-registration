2c234ca9126f720a2200c42adf803bdb3741a348
Shree Nivas Kushwah <shri.mca2010@gmail.com>
2024-07-17 22:04:51 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { RegistrationPublicComponent } from './registration-public/registration-public.component';
 import { GetRegistrationStatisticsComponent } from './registration-reports/get-registration-statistics/get-registration-statistics.component';
+import { LoginNowComponent } from './login/login-now/login-now.component';
+import { LoginSignupComponent } from './login/login-signup/login-signup.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
         {path: "registration",component: RegistrationPublicComponent},
-        {path: "registration-statistics",component: GetRegistrationStatisticsComponent}
+        {path: "registration-statistics",component: GetRegistrationStatisticsComponent},
+        {path: "login-now",component: LoginNowComponent},
+        {path: "signup",component: LoginSignupComponent},
+        {path: "forgot-password",component: ForgotPasswordComponent},
+        {path: "**",redirectTo:"/"}
     ];
