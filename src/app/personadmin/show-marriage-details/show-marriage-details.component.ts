eef3c36922deaf1281313504d01ce7100b1ff05b
Shree Nivas Kushwah <shri.mca2010@gmail.com>
2024-07-17 22:13:30 +0530

    person admin added

---
+++ src/app/personadmin/show-marriage-details/show-marriage-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-marriage-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-marriage-details.component.html',
+  styleUrl: './show-marriage-details.component.css'
+})
+export class ShowMarriageDetailsComponent {
+
+}
---
+++ src/app/personadmin/update-marriage-details/update-marriage-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-marriage-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-marriage-details.component.html',
+  styleUrl: './update-marriage-details.component.css'
+})
+export class UpdateMarriageDetailsComponent {
+
+}
