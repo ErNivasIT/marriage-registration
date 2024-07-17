bc8fc34628019e683c71b05f4ab0e5a036ae5626
Shree Nivas Kushwah <shri.mca2010@gmail.com>
2024-07-17 21:26:55 +0530

    first commit

---
+++ src/app/registration-public/registration-public.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-public',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-public.component.html',
+  styleUrl: './registration-public.component.css'
+})
+export class RegistrationPublicComponent {
+
+}
